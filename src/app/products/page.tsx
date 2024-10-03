e7837925574d99610f29c9d8075b8e6f34068a89
Gunjan Jain <jain10gunjan@gmail.com>
2024-10-03 10:20:10 +0530

    ff

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -75,14 +75,6 @@ const ProductsPage: React.FC = () => {
                             ))}
                           </ul>
                         </div>
-                        <div className="flex justify-center">
-                          <a
-                            href={`/products/${data.slug}`}
-                            className="inline-flex text-white bg-orange-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-                          >
-                            Explore
-                          </a>
-                        </div>
                       </div>
                     </div>
                   </section>
